f3470c5e7a6b9273b0c2b8e818c74424a9375051
ss7m <Sean.Y.Shu@newegg.com>
2019-08-30 17:35:42 +0800

    feat: update

--- src/http.js
+++ src/http.js
@@ -1,13 +1,24 @@
-const http = require('http');
-const data = {
-    name:'node demo',
-    date:new Date().getDate(),
-    value:520
-}
-http.createServer((req,res) => {
-   res.setHeader('Content-Type', 'application/json;charset=utf-8');
-   const result = JSON.stringify(data);
-   res.end(result);
-}).listen(8080,() => {
-    console.log('Listened in port 8080')
-})
+const http = require("http");
+const url = require("url");
+const qs = require("querystring");
+const getUserData = userId => ({
+  userId,
+  name: "node demo",
+  date: new Date().getDate()
+});
+http
+  .createServer((req, res) => {
+    res.setHeader("Content-Type", "application/json;charset=utf-8");
+    const reqUrl = url.parse(req.url);
+    if (reqUrl.pathname === "/index") {
+      const uid = qs.parse(reqUrl.query).userId;
+      const result = JSON.stringify(getUserData(uid));
+      res.end(result);
+    } else {
+      res.writeHead(404);
+      res.end("NotFound");
+    }
+  })
+  .listen(8080, () => {
+    console.log("Listened in port 8080");
+  });
